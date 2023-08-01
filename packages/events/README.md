fd0dfb9592ef29294f064600d95dad5c67c2aaa5
Andy Chentsov <chentsov@gmail.com>
2023-08-01 16:06:32 +0300

    Document @simulacron/events package

--- packages/events/README.md
+++ packages/events/README.md
@@ -1,4 +1,30 @@
 
-# LitWidget events helpers
+# LitWidget conditional events helpers
 
-Helper functions for LitWidget events.
+Helper functions for LitWidget [conditional events](https://github.com/andyduke/lit-widget/blob/master/packages/lit-widget/DOCUMENTATION.md#conditional-events).
+
+* `keydown()` - **keydown** event helper;
+* `keyup()` - **keyup** event helper;
+* `keypress()` - **keypress** event helper.
+
+You can specify one or more keyboard shortcuts:
+```js
+class SampleWidget extends LitWidget {
+
+  @onEvent('search-field', keydown('esc'))
+  cancel(event) {
+    ...
+  }
+
+  // or
+
+  @onEvent('search-field', keydown(['esc', 'ctrl+k']))
+  cancel(event) {
+    ...
+  }
+
+}
+```
+
+For the keyboard shortcut format, see
+[KeyboardShortcuts](https://github.com/andyduke/lit-widget/tree/master/packages/keyboard).
--- packages/events/src/events-helpers.js
+++ packages/events/src/events-helpers.js
@@ -1,5 +1,21 @@
 import { KeyboardShortcuts } from '@simulacron/keyboard';
 
+/**
+ * **Keydown** event helper for [LitWidget](https://github.com/andyduke/lit-widget).
+ *
+ * You can specify one or more keyboard shortcuts:
+ * ```js
+ * keydown('mod+s')
+ * // or
+ * keydown(['mod+s', 'ctrl+k'])
+ * ```
+ *
+ * For the keyboard shortcut format, see
+ * [KeyboardShortcuts](https://github.com/andyduke/lit-widget/tree/master/packages/keyboard).
+ *
+ * @param {string|string[]} shortcut
+ * @returns {object} Conditional event structure
+ */
 export function keydown(shortcut) {
   const shortcuts = new KeyboardShortcuts(shortcut);
   return {
@@ -8,6 +24,22 @@ export function keydown(shortcut) {
   }
 }
 
+/**
+ * **Keyup** event helper for [LitWidget](https://github.com/andyduke/lit-widget).
+ *
+ * You can specify one or more keyboard shortcuts:
+ * ```js
+ * keyup('mod+s')
+ * // or
+ * keyup(['mod+s', 'ctrl+k'])
+ * ```
+ *
+ * For the keyboard shortcut format, see
+ * [KeyboardShortcuts](https://github.com/andyduke/lit-widget/tree/master/packages/keyboard).
+ *
+ * @param {string|string[]} shortcut
+ * @returns {object} Conditional event structure
+ */
 export function keyup(shortcut) {
   const shortcuts = new KeyboardShortcuts(shortcut);
   return {
@@ -16,6 +48,22 @@ export function keyup(shortcut) {
   }
 }
 
+/**
+ * **Keypress** event helper for [LitWidget](https://github.com/andyduke/lit-widget).
+ *
+ * You can specify one or more keyboard shortcuts:
+ * ```js
+ * keypress('mod+s')
+ * // or
+ * keypress(['mod+s', 'ctrl+k'])
+ * ```
+ *
+ * For the keyboard shortcut format, see
+ * [KeyboardShortcuts](https://github.com/andyduke/lit-widget/tree/master/packages/keyboard).
+ *
+ * @param {string|string[]} shortcut
+ * @returns {object} Conditional event structure
+ */
 export function keypress(shortcut) {
   const shortcuts = new KeyboardShortcuts(shortcut);
   return {
