624f10d262bf8c930481a8229f60a973df9a7303
Andy Chentsov <chentsov@gmail.com>
2023-08-01 15:25:08 +0300

    Document @simulacron/keyboard package

--- packages/keyboard/README.md
+++ packages/keyboard/README.md
@@ -3,3 +3,73 @@
 
 Helper classes and functions for working with the keyboard.
 
+
+## KeyboardShortcut
+
+Class for handling keyboard shortcut. Typically used in browser keyboard event handlers.
+
+You must instantiate the class with the keyboard shortcut,
+and then use the `isMatchEvent()` method to test for the keyboard event:
+```js
+const shortcut = new KeyboardShortcut('mod+s');
+
+element.addEventListener('keydown', function(event) {
+  if (shortcut.isMatch(event)) {
+    // Do action...
+  }
+});
+```
+
+### Keyboard shortcuts format
+
+Keyboard shortcuts are specified using a simple format such as `'modifier+key'`,
+`'modifier+modifier+key'`, or simply `'key'`.
+
+`Modifiers` can be:
+- `mod` - cross-platform modifier, Ctrl on Windows and Cmd on Mac;
+- `ctrl`, `ctl` - Control on Windows;
+- `alt`, `opt`, `option` - Alt;
+- `shift` - Shift;
+- `meta` - cross-platform modifier, Win on Windows, Cmd on Mac;
+- `win`, `windows` - Win on Windows;
+- `cmd`, `command` - Cmd on Mac.
+
+`Key` is any value from the predefined `KeyboardEvent.key` keys, specified in any case.
+
+The class provides some aliases for the keys from the `KeyboardEvent`, such as:
+- `esc` - 'Escape' key;
+- `return` - 'Enter' key;
+- `space`, `spacebar` - 'spacebar' key;
+- `add` - '+' key;
+- `break` - 'Pause' key;
+- `ins` - 'Insert' key;
+- `del` - 'Delete' key;
+- `up` - 'ArrowUp' key;
+- `down` - 'ArrowDown' key;
+- `left` - 'ArrowLeft' key;
+- `right` - 'ArrowRight' key.
+
+> This class is inspired by the `is-hotkey` package from Ian Storm Taylor <https://github.com/ianstormtaylor>
+
+
+## KeyboardShortcuts
+
+A class for handling *multiple* keyboard shortcuts.
+
+It is used when it is necessary to use one handler
+for several keyboard shortcuts.
+
+```js
+const shortcuts = new KeyboardShortcuts([
+  'mod+s',
+  'ctrl+k',
+]);
+
+element.addEventListener('keydown', function(event) {
+  if (shortcuts.isMatch(event)) {
+    // Do action...
+  }
+});
+```
+
+See [KeyboardShortcut](#keyboardshortcut) for details.
--- packages/keyboard/src/keyboard.js
+++ packages/keyboard/src/keyboard.js
@@ -1,5 +1,52 @@
-// https://github.com/ianstormtaylor/is-hotkey/blob/master/src/index.js
-
+/**
+ * Class for handling keyboard shortcut. Typically used in browser
+ * keyboard event handlers.
+ *
+ * You must instantiate the class with the keyboard shortcut,
+ * and then use the `isMatchEvent()` method to test for the keyboard event.
+ *
+ * @example
+ * ```js
+ * const shortcut = new KeyboardShortcut('mod+s');
+ *
+ * element.addEventListener('keydown', function(event) {
+ *   if (shortcut.isMatch(event)) {
+ *     // Do action...
+ *   }
+ * });
+ * ```
+ *
+ * Keyboard shortcuts are specified using a simple format such as `'modifier+key'`,
+ * `'modifier+modifier+key'`, or simply `'key'`.
+ *
+ * `Modifiers` can be:
+ * - `mod` - cross-platform modifier, Ctrl on Windows and Cmd on Mac;
+ * - `ctrl`, `ctl` - Control on Windows;
+ * - `alt`, `opt`, `option` - Alt;
+ * - `shift` - Shift;
+ * - `meta` - cross-platform modifier, Win on Windows, Cmd on Mac;
+ * - `win`, `windows` - Win on Windows;
+ * - `cmd`, `command` - Cmd on Mac.
+ *
+ * `Key` is any value from the predefined `KeyboardEvent.key` keys, specified in any case.
+ *
+ * The class provides some aliases for the keys from the `KeyboardEvent`, such as:
+ * - `esc` - 'Escape' key;
+ * - `return` - 'Enter' key;
+ * - `space`, `spacebar` - 'spacebar' key;
+ * - `add` - '+' key;
+ * - `break` - 'Pause' key;
+ * - `ins` - 'Insert' key;
+ * - `del` - 'Delete' key;
+ * - `up` - 'ArrowUp' key;
+ * - `down` - 'ArrowDown' key;
+ * - `left` - 'ArrowLeft' key;
+ * - `right` - 'ArrowRight' key.
+ *
+ * ---
+ *
+ * This class is inspired by the `is-hotkey` package from Ian Storm Taylor <https://github.com/ianstormtaylor>
+ */
 export class KeyboardShortcut {
 
   constructor(shortcut, {useKey = true} = {}) {
@@ -145,6 +192,12 @@ export class KeyboardShortcut {
     return result;
   }
 
+  /**
+   * Checks if the keyboard event matches the class's keyboard shortcut.
+   *
+   * @param {KeyboardEvent} event
+   * @returns {boolean}
+   */
   isMatchEvent(event) {
     for (const key in this.shortcut) {
       const expected = this.shortcut[key];
@@ -185,6 +238,28 @@ export class KeyboardShortcut {
 
 }
 
+/**
+ * A class for handling multiple keyboard shortcuts.
+ *
+ * It is used when it is necessary to use one handler
+ * for several keyboard shortcuts.
+ *
+ * @example
+ * ```js
+ * const shortcuts = new KeyboardShortcuts([
+ *   'mod+s',
+ *   'ctrl+k',
+ * ]);
+ *
+ * element.addEventListener('keydown', function(event) {
+ *   if (shortcuts.isMatch(event)) {
+ *     // Do action...
+ *   }
+ * });
+ * ```
+ *
+ * @see {@link KeyboardShortcut}
+ */
 export class KeyboardShortcuts {
 
   constructor(shortcuts) {
@@ -194,11 +269,6 @@ export class KeyboardShortcuts {
 
   isMatchEvent(event) {
     return this.shortcuts.some((s) => s.isMatchEvent(event));
-    // return this.shortcuts.some((s) => {
-    //   const result = s.isMatchEvent(event);
-    //   console.log('is', event, 'match', s, '=', result);
-    //   return result;
-    // });
   }
 
 }
